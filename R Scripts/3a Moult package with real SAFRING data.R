f5fcf8fb33dc5d7a410ca99cabc5df379e6697a8
RooIrons <bexirons@gmail.com>
2022-04-27 13:27:03 +0200

    Sorting folders and replacing the old script with the new (3a).

--- R Scripts/3a Moult package with real SAFRING data.R
+++ R Scripts/3a Moult package with real SAFRING data.R
@@ -2,20 +2,27 @@
 
 # 14 April 2022
 # Assumes data has been read and cleaned (sheet 1)
+# 26 April update to read data directly here
 
-install.packages("moult")
 library(moult)
+source(
+  file = "function_read_clean_safring_data.R"
+)
+
+ringing_data <- safring_get_clean_data(Spp_number = 760)
 
 if (is.numeric(ringing_data$Moult)) {
   scores <- format(ringing_data$Moult, scientific = FALSE, trim = TRUE)
 } else {
   scores <- ringing_data$Moult
 }
+scores
 mscores <- substr(scores, 1, 10)
 unique(mscores)
-feather.mass <- c(10.4, 10.8, 11.5, 12.8, 14.4, 15.6, 16.3, 15.7, 15.7, 10.0)
+# See this paper: https://www.tandfonline.com/doi/abs/10.2989/00306525.2010.455821
+feather.mass <- c(9, 9.7, 10.3, 11, 11.9, 12.1, 12.2, 12, 10, 1.8) # SDCS
 ## convert moult scores to proportion of feather mass grown
-ringing_data$pfmg <- ms2pfmg(mscores, feather.mass)
+ringing_data$pfmg <- ms2pfmg(mscores, feather.mass) #ms2pfmg from moult package
 str(ringing_data$pfmg)
 hist(ringing_data$pfmg)
 ringing_data$day <- date2days(ringing_data$Startdate, dateformat = "yyyy-mm-dd", startmonth = 8)
@@ -28,7 +35,7 @@ ringing_data$ssex <- as.factor(ssex)
 
 
 ## model with duration and mean start date of moult depending on sex
-mmf <- moult(pfmg ~ day | ssex | ssex, data = ringing_data, type = 1)
+mmf <- moult(pfmg ~ day | ssex | ssex, data = ringing_data, type = 3)
 summary(mmf)
 ## predict duration and start of moult (then both) for males and females
 ssex <- c("male", "female")
